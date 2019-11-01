cdf835c64f9e9a6324471f4fbd93f8d347a21bc2
Joe Moudrik <j.moudrik@gmail.com>
2019-11-01 13:10:37 +0100

    index: dalsi 3 kalkulacky

--- kalkulacka.js
+++ kalkulacka.js
@@ -13,12 +13,48 @@ function AppViewModel() {
 		return ret;
 	}, this);
 
+	this.vepr_hovezi_uspora= ko.observable(20);
+
+	this.vepr_hovezi_uspora_kg = ko.computed(function() {
+	    var vepr_hovezi_celkem_avg = (DATA.mat_cr_avg_veprovy * DATA.veprovy_kg_co2
+	                                + DATA.mat_cr_avg_hovezi * DATA.hovezi_kg_co2);
+	    //console.log(vepr_hovezi_celkem_avg + DATA.mat_cr_avg_kureci * DATA.kureci_kg_co2);
+
+	    var kure_misto_toho =  DATA.kureci_kg_co2 * (
+	                    // kolik veprovyho & hoveziho si odpustim
+	                DATA.mat_cr_avg_veprovy * this.vepr_hovezi_uspora() / 100
+	                + DATA.mat_cr_avg_hovezi * this.vepr_hovezi_uspora() / 100);
+	    //console.log(kure_misto_toho);
+
+		var jim_ted = (100 - this.vepr_hovezi_uspora()) / 100 * vepr_hovezi_celkem_avg + kure_misto_toho;
+	    //console.log(jim_ted);
+
+		// formulujem jako uspora, tzn to co ji prumerny cech - to co jim ted
+		return vepr_hovezi_celkem_avg - jim_ted;
+	}, this);
 
 	this.teplota_snizena = ko.observable(2);
 
 	this.teplota_uspora_kg = ko.computed(function() {
 		// 1 stupen ~= 6% uspory energie
 		var ret = this.teplota_snizena() * 0.06 * DATA.mat_cr_avg_teplo;
+		return ret;
+	}, this);
+
+	this.auto_mhd_uspora = ko.observable(20);
+
+	this.auto_mhd_uspora_kg = ko.computed(function() {
+		var usetreno_za_auto = this.auto_mhd_uspora() / 100 * DATA.cr_avg_doprava_auto_est ;
+        var pridano_za_mhd = this.auto_mhd_uspora() / 100 * DATA.cr_auto_mobility_per_cap * DATA.public_co2_km_avg;
+		return usetreno_za_auto - pridano_za_mhd;
+	}, this);
+
+
+	this.maso_mleko_uspora = ko.observable(2);
+
+	this.maso_mleko_uspora_kg = ko.computed(function() {
+	    var maso_mleko_celkem = ( DATA.jidlo_maso_ratio + DATA.jidlo_mleko_ratio ) * DATA.cr_avg_jidlo_est;
+		var ret = this.maso_mleko_uspora() / 7 * maso_mleko_celkem;
 		return ret;
 	}, this);
 
